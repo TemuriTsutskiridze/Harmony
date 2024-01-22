638d5444275369a8c155cfa4a7cd913cfe510683
Ctato1 <dznelashvilitato@gmail.com>
2024-01-23 01:15:10 +0400

    add similar categories

--- pages/blog/blog.js
+++ pages/blog/blog.js
@@ -89,22 +89,90 @@ darkTheme.addEventListener("click", cancelHeaderContainer);
 automaticLoad();
 // blog html
 const main = document.getElementById("main__blog");
-function filterDataByCategoryTitles(dataArray, categoryTitles) {
+const blogSimilar = document.getElementById("blog__similar");
 
-  // we need to fix that
-  let a =dataArray.filter((item) =>
-    item.categories.some((category) => categoryTitles.includes(category.title))
-  );
-  console.log(a);
-}
-const similarStats =async (array)=>{
-  const resp = await fetch(
-    `https://george.pythonanywhere.com/api/blogs/`
+function filterDataByCategoryTitles(dataArray, categoryTitles) {
+  let result = [];
+
+  for (let i = 0; i < categoryTitles.length; i++) {
+    let filteredArray = dataArray.filter((cat) =>
+      cat.categories.some(
+        (element) => element.title === categoryTitles[i].title
+      )
     );
-    const data = await resp.json();
-    const filteredData = filterDataByCategoryTitles(data, array);
-  // console.log(filteredData);
+    result = result.concat(filteredArray);
+  }
+  return result;
 }
+
+// new blog page
+
+function loadNewBlog(newId) {
+  localStorage.setItem("blogId", newId);
+  window.scrollTo({ top: 0, behavior: "smooth" });
+  // smooth scroll
+  setTimeout(function () {
+    document.location.reload(true);
+  }, 600); // Adjust the delay as needed
+}
+
+// add in HTML
+function addSimilarBlogs(data) {
+  for (let blog in data) {
+    let element = data[blog];
+    let categoryDiv = "";
+
+    for (let i = 0; i < element.categories.length; i++) {
+      // Append each category HTML to the existing string
+      categoryDiv += `
+        <div class="blog__info-category" style="background-color: ${element.categories[i].background_color}">${element.categories[i].title}</div>
+      `;
+    }
+    blogSimilar.innerHTML += `
+    <div class="similar__cards">
+    <img
+      src=${element.image}
+      alt="Blog image"
+    />
+    <p class="author">${element.author}</p>
+    <p class="date">${element.publish_date.substring(0, 10)}</p>
+    <h2>${element.title}</h2>
+    <div class="category-container">
+      ${categoryDiv}
+    </div>
+    <p class="description">
+    ${element.description}
+    </p>
+    <div class="view-full" onclick="loadNewBlog(${element.id})">
+      <span>სრულად ნახვა</span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+        fill="none"
+      >
+        <path
+          d="M5.93451 13.0052C5.64162 13.2981 5.64162 13.773 5.93451 14.0659C6.22741 14.3587 6.70228 14.3587 6.99517 14.0659L5.93451 13.0052ZM14.2859 6.46446C14.2859 6.05024 13.9501 5.71445 13.5359 5.71446L6.78591 5.71445C6.3717 5.71445 6.03591 6.05024 6.03591 6.46445C6.03591 6.87867 6.3717 7.21445 6.78591 7.21445H12.7859V13.2145C12.7859 13.6287 13.1217 13.9645 13.5359 13.9645C13.9501 13.9645 14.2859 13.6287 14.2859 13.2145L14.2859 6.46446ZM6.99517 14.0659L14.0662 6.99478L13.0056 5.93412L5.93451 13.0052L6.99517 14.0659Z"
+          fill="#5D37F3"
+        />
+      </svg>
+    </div>
+  </div>
+    
+    `;
+  }
+}
+const similarStats = async (array) => {
+  try {
+    const resp = await fetch(`https://george.pythonanywhere.com/api/blogs/`);
+    const data = await resp.json();
+    let filteredByTitle = filterDataByCategoryTitles(data, array.categories);
+    addSimilarBlogs(filteredByTitle);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
 
 const loadBlog = async () => {
   const blogId = localStorage.getItem("blogId");
@@ -112,7 +180,7 @@ const loadBlog = async () => {
     `https://george.pythonanywhere.com/api/blogs/${blogId}`
   );
   const data = await resp.json();
-  let categoryDiv = '';
+  let categoryDiv = "";
 
   for (let i = 0; i < data.categories.length; i++) {
     // Append each category HTML to the existing string
@@ -120,20 +188,22 @@ const loadBlog = async () => {
       <div class="blog__info-category" style="background-color: ${data.categories[i].background_color}">${data.categories[i].title}</div>
     `;
   }
-  
-  similarStats(data.categories)
- 
+
+  similarStats(data);
+  console.log(data);
   main.innerHTML = `
   <div class="blog__img">
         <img
-          src="https://s3-alpha-sig.figma.com/img/f61f/fadf/53c6b0eca0d67f6b1b3b647378282151?Expires=1706486400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=JM6VdU6mXXpbLsl-6LSBj~otjSVxX8e~lbuukxObvHN4VaQ6C2XXf~4T2i9rChTtl7gb515i9i99mo9Ij293mVH83vUCK52NHio9c8iBM7uJDt~hPNkcuSyNywfzsTfR29~gXVJkOe-5KpyinuNlM3NWFlnnz~2~JjInsGmbWtOyqsU6kf~v4iKKLRz6IVOW-3bDeSdMfo2BvkIrOF8LoJZH31LRgqj0mWK4pOZj92dFz0pFzh5yPe6PI7SNT4fDbTxHan6KnNbCRRWm81bhCUeeYk40jQujWCwpOLTau0QhT99iT8pBhWTtxhrEKhTWKNlvmHFyb531mRrq1lKQvw__"
+          src="${data.image}"
           alt="blog image"
         />
       </div>
 
       <div class="blog__info">
         <h3>${data.author}</h3>
-        <p><span>${data.publish_date.substring(0,10)}</span> • ${data.email === null ? '' : data.email}</p>
+        <p><span>${data.publish_date.substring(0, 10)}</span> • ${
+    data.email === null ? "" : data.email
+    }</p>
         <h1>${data.title}</h1>
         <div class="blog__info-categories">
           ${categoryDiv}
