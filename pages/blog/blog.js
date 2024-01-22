898ca4ce863bbc4c03ab4d980dde5151fca845e9
Ctato1 <dznelashvilitato@gmail.com>
2024-01-22 04:16:00 +0400

    add blogPage

---
+++ pages/blog/blog.js
@@ -0,0 +1,91 @@
+const headerBtn = document.getElementById("header__btn");
+const blogBtn = document.getElementById("blog__btn");
+const darkTheme = document.getElementById("dark__theme");
+const headerContainer = document.getElementById("header__container");
+const headerContainerValid = document.getElementById("header__container-valid");
+const cancelBtn = document.getElementById("cancel__btn");
+const cancelBtnValid = document.getElementById("cancel__btn-valid");
+const signInBtn = document.getElementById("signin__btn");
+const confirmBtn = document.getElementById("confirm__btn");
+const headerErrorText = document.getElementById("header__error");
+// user input
+const userInput = document.getElementById("user__email");
+
+let userLoged = true;
+// login function
+const headerBtnClick = () => {
+  darkTheme.classList.add("active");
+  headerContainer.classList.add("active");
+};
+
+// remove login container
+const cancelHeaderContainer = () => {
+  darkTheme.classList.remove("active");
+  headerContainer.classList.remove("active");
+  headerContainerValid.classList.remove("active");
+};
+//
+
+// checking validation
+const checkValidation = async () => {
+  headerErrorText.classList.remove("active");
+  try {
+    const response = await fetch(
+      "https://george.pythonanywhere.com/api/login/",
+      {
+        method: "POST",
+        headers: {
+          accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: `${userInput.value}`,
+        }),
+      }
+    );
+    if (!response.ok) throw new Error("something went wrong");
+    const data = await response.json();
+    localStorage.setItem("token", data.token);
+    const checkToken = localStorage.getItem('token');
+    if(checkToken){
+      headerContainerValid.classList.add("active");
+      headerContainer.classList.remove("active");
+      headerBtn.style.display = "none";
+      blogBtn.style.display = "block";
+      blogBtn.addEventListener('click',()=>{
+        window.location.href = '../addNewBlog/addNewBlog.html'
+      })
+    }
+  } catch (error) {
+    headerErrorText.classList.add("active");
+  }
+
+
+};
+
+// automatic load
+const automaticLoad = () => {
+  let localUser = localStorage.getItem("token");
+  if (localUser !== null) {
+    headerBtn.style.display = "none";
+    blogBtn.style.display = "block";
+    blogBtn.addEventListener('click',()=>{
+      window.location.href = '../addNewBlog/addNewBlog.html'
+    })
+  }
+};
+
+// 1st click
+headerBtn.addEventListener("click", headerBtnClick);
+// 2nd click
+signInBtn.addEventListener("click", checkValidation);
+// 3d click
+confirmBtn.addEventListener("click", cancelHeaderContainer);
+
+// click to cancel the container of header
+cancelBtn.addEventListener("click", cancelHeaderContainer);
+cancelBtnValid.addEventListener("click", cancelHeaderContainer);
+darkTheme.addEventListener("click", cancelHeaderContainer);
+
+// check is there is user in localstorage
+automaticLoad();
